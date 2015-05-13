86da619663f1614389e080c20066a3693cba4944
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2015-05-12 19:59:37 -0600

    Fixed indentation.

--- shell.py
+++ shell.py
@@ -10,29 +10,28 @@ def start():
     server.listen()
 
 if __name__ == '__main__':
-  from prompt_toolkit.contrib.shortcuts import get_input
-  import threading
-  
-  #Welcome message
-  print("\n--------------------------------------------------\r\nRhonda: Router project \n\n Available Commands:")
-  print("\tstart\n\tquit")
-  print("--------------------------------------------------\n\n")
-  
-  #Start flag
-  served = None
-  
-  #Console loop
-  while True:
-    parts = get_input('clsfy> ').lower().split()
-    #Check which command
-    if parts[0] == 'quit':
-      break
-    elif parts[0] == 'start':
-      if not served:
-        served = threading.Thread(target=start)
-        served.daemon = True
-        served.start()
-      else:
-        print("You can only start the router once.");
-  
-  print("Good bye! — Rhonda")
+    from prompt_toolkit.contrib.shortcuts import get_input
+    import threading
+
+    #Welcome message
+    print("\n--------------------------------------------------\r\nRhonda: Router project \n\n Available Commands:")
+    print("\tstart\n\tquit")
+    print("--------------------------------------------------\n\n")
+
+    #Start flag
+    served = None
+
+    #Console loop
+    while True:
+        parts = get_input('clsfy> ').lower().split()
+        #Check which command
+        if parts[0] == 'quit':
+            break
+        elif parts[0] == 'start':
+            if not served:
+                served = threading.Thread(target=start)
+                served.daemon = True
+                served.start()
+        else:
+            print("You can only start the router once.");
+    print("Good bye! — Rhonda")
