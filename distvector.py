6056b9b74ba2e2d63d31b4c365f5f63e373f2d86
unknown <ben.mmr199@gmail.com>
2015-05-15 17:36:12 -0600

    Dist vector

--- distvector.py
+++ distvector.py
@@ -1,14 +1,27 @@
 from wendy import Worker
+import time
 
 class DVWorker(Worker):
     
     def __init__(self, sckt, address):
         # init things
-        super().__init__(sckt, address)
+				retry = 3
+				super().__init__(sckt, address)
     
     def start(self):
+		
         print('Connected by:', self.address)
+
         while True:
-            data = self.recv()
-            print('Received cuz yolo %d bytes' % len(data))
-            self.send(data)
+						
+						data = self.recv()
+						if(!data):
+								self.send("Keep alive")
+						else:
+							retry-=1
+						if(retry==0):
+							exit()
+						time.sleep(30) 
+
+       
+						
